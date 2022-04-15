2b610bb1bcfc35fe3de28a53263ed3b63dc9f49e
SeNicko <sebastianflajszer@gmail.com>
2022-04-15 16:24:31 +0200

    Fix indentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-  "time"
+	"time"
 
 	"github.com/senicko/run-api/pkg/api"
 	"github.com/senicko/run-api/pkg/controllers"
@@ -11,23 +11,23 @@ import (
 )
 
 func main() {
-  logger, _ := zap.NewProduction()
-  defer logger.Sync()
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
 
-  sandboxService, err := services.NewSandboxService(logger, services.SandboxServiceConfig{
-    PoolWorkers: 8,
-  })
-  if err != nil {
-    logger.Error("failed to create sandbox service")
-  }
+	sandboxService, err := services.NewSandboxService(logger, services.SandboxServiceConfig{
+		PoolWorkers: 8,
+	})
+	if err != nil {
+		logger.Error("failed to create sandbox service")
+	}
 
-  sandboxController := controllers.NewSandboxController(logger, sandboxService)
+	sandboxController := controllers.NewSandboxController(logger, sandboxService)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/run", api.Method(http.MethodPost, sandboxController.Run))
 
 	server := &http.Server{
-    Addr:         ":8080",
+		Addr:         ":8080",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
@@ -36,6 +36,6 @@ func main() {
 
 	logger.Info("Starting on http://localhost:8080")
 	if err := server.ListenAndServe(); err != nil {
-    logger.Fatal("failed to start")
+		logger.Fatal("failed to start")
 	}
 }
